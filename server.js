9b0460864a87215e865cb5eee6eeb270ca0a950d
MhideTech <okefolahanolamide2006@gmail.com>
2025-10-21 13:40:11 +0100

    done

--- server.js
+++ server.js
@@ -21,8 +21,6 @@ if (!process.env.MONGO_URI) {
 
 mongoose
   .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
     serverSelectionTimeoutMS: 10000,
   })
   .then(() => console.log("MongoDB Connected"))
